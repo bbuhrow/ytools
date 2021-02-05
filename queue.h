a1e70dc127da1c4f4c456a103f8296ad630021e4
bbuhrow <59025993+bbuhrow@users.noreply.github.com>
2021-02-05 09:28:10 -0600

    getting started putting together a set of common tools/utilities

---
+++ queue.h
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------
+MIT License
+
+Copyright (c) 2021 bbuhrow
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+----------------------------------------------------------------------*/
+#ifndef _QUEUE_H
+#define _QUEUE_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct
+{
+	uint32_t *Q;
+	uint32_t len;
+	uint32_t sz;
+	uint32_t head;
+	uint32_t tail;
+} Queue_t;
+
+void clearQueue(Queue_t *Q);
+uint32_t peekqueue(Queue_t *Q);
+uint32_t dequeue(Queue_t *Q);
+void enqueue(Queue_t *Q, uint32_t e);
+Queue_t * newQueue(uint32_t sz);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
